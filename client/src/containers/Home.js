5945b312a4ad9cfec0f55d4f5d2750da7343d874
Kapil <k.d.n.pau@se15.qmul.ac.uk>
2019-01-01 19:22:40 +0000

    Fixed paths

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -27,7 +27,7 @@ export default class Home extends React.Component {
         {
             this.props.history.push('/app/login');
         }
-        fetch('/list', {
+        fetch('/fire/list', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
